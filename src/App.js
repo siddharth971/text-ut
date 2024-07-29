5ff44ba9d041468d9c22dbd899e4a5fb7267e1cb
siddharth971 <siddharthsiddharth971@gmail.com>
2024-07-29 22:48:50 +0530

    Changes to be committed:

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 
 import './App.css';
+import { Navbar } from './components/Navbar';
 
 
 function App() {
   return (
     <>
+    <Navbar  title="Hello" page="gallery"></Navbar>
     </>
   );
 }
