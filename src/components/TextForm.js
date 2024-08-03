3987726a76d0158c9010e22d6d165e3e6b167f06
siddharth971 <siddharthsiddharth971@gmail.com>
2024-08-03 11:00:04 +0530

    Changes to be committed:

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import React from "react";
 
 function TextForm(props) {
+    function handleUpchange() {
+        console.log("button is click")
+    }
+    const [text, settext] = useState('enter text hare')
     return (
-        <> 
+        <>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" id="myBox" rows="8"></textarea>
+                <textarea className="form-control" value={text} onChange={handleUpchange} id="myBox" rows="8"></textarea>
             </div>
         </>
     );
