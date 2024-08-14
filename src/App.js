7eb46a15b820ccc8076254dd2f3158bb0c9b128a
siddharth971 <siddharthsiddharth971@gmail.com>
2024-08-14 11:16:09 +0530

    Changes to be committed:

--- src/App.js
+++ src/App.js
@@ -2,30 +2,30 @@ import { useState } from 'react';
 import './App.css';
 import About from './components/About';
 import Alert from './components/Alert';
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
 import TextForm from './components/TextForm';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
 
 function App(props) {
   const [alert, setAlert] = useState("this is sample error");
- 
 
   return (
-    <>
-
-      <Navbar className='my-0' alert={setAlert} title="blog" page="Text-Home" />
-      <Alert alert={alert} />
+    <Router>
+      <Navbar className='my-0' alert={setAlert} title="Home" page="Text-Home" />
       <div className='container my-3'>
-
-        <TextForm heading='Enter text to analyze' />
-
+        <Routes>
+          <Route path="/alert" element={<Alert alert={alert} />} />
+          <Route path="/textform" element={<TextForm heading='Enter text to analyze' />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/" element={<TextForm heading='Enter text to analyze' />} /> {/* Default route */}
+        </Routes>
       </div>
-      <About />
-    </>
-
-
+    </Router>
   );
 }
-
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,71 +1,58 @@
-
 import React, { useState } from "react";
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom'; // Ensure to import Link
 
 function Navbar(props) {
     const [bg, setBg] = useState("dark");
-    const [title,setitle]= useState("white")
+    const [title, setTitle] = useState("white"); // Fixed typo here
 
-    const lt = ()=> {
+    const toggleTheme = () => {
         if (bg === "white") {
             setBg("dark");
-            setitle("white")
-            document.title = "text-ut dark thime "
-           
-        }
-        else {
+            setTitle("white");
+            document.title = "Text - Dark Theme"; // Fixed typo in title
+        } else {
             setBg("white");
-            setitle("black")
-            document.title = "text-ut light thime "
+            setTitle("black");
+            document.title = "Text - Light Theme"; // Fixed typo in title
         }
-    }
+    };
+
     return (
-        <>
-            <nav className={`navbar navbar-expand-lg bg-${bg} text-${title} `} data-bs-theme={`${bg}`}>
-                <div className="container-fluid mx-5 ">
-                    <a className="navbar-brand text" href="/">{props.page}</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/">{props.title}</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/">Link</a>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown
-                                </a>
-                                <ul className="dropdown-menu">
-                                    <li><a className="dropdown-item" href="/">Action</a></li>
-                                    <li><a className="dropdown-item" href="/">Another action</a></li>
-                                    <li><a className="dropdown-item" href="/">Something else here</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                        <div className={`form-check form-switch `}>
-                            <input className="form-check-input" type="checkbox" role="switch" onClick={lt} id="flexSwitchCheckDefault" />
-                            <label className={`form-check-label text-${title}`} htmlFor="flexSwitchCheckDefault">{title}</label>
-                        </div>
+        <nav className={`navbar navbar-expand-lg bg-${bg} text-${title}`} data-bs-theme={bg}>
+            <div className="container-fluid mx-5">
+                <Link className="navbar-brand text" to="/">{props.page}</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link active" aria-current="page" to="/">{props.title}</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/about">About</Link> {/* Path should be lowercase for consistency */}
+                        </li>
+                    </ul>
+                    <div className={`form-check form-switch`}>
+                        <input className="form-check-input" type="checkbox" role="switch" onClick={toggleTheme} id="flexSwitchCheckDefault" />
+                        <label className={`form-check-label text-${title}`} htmlFor="flexSwitchCheckDefault">{title === "white" ? "Dark" : "Light"} Mode</label>
                     </div>
                 </div>
-            </nav>
-        </>
-    )
-
+            </div>
+        </nav>
+    );
 }
 
 Navbar.propTypes = {
     title: PropTypes.string.isRequired,
     page: PropTypes.string.isRequired
-}
+};
 
+// Uncomment if you want to provide default values
 // Navbar.defaultProps = {
-//     title: "this is title",
-//     page : "page"
+//     title: "Default Title",
+//     page: "Default Page"
 // }
 
 export default Navbar;
